9c1b694a57ba221894f215557305f1abe7f37c99
Swetha Angara <swetha.angara5689@gmail.com>
2021-03-30 02:28:05 -0400

    no constraint model

--- src/main/java/com/quickshift/quickshiftbackend/service/SolverController.java
+++ src/main/java/com/quickshift/quickshiftbackend/service/SolverController.java
@@ -2,6 +2,7 @@ package com.quickshift.quickshiftbackend.service;
 
 
 import com.quickshift.quickshiftbackend.models.*;
+import com.quickshift.quickshiftbackend.solver.ScheduleTableConstraintProvider;
 import org.optaplanner.core.api.score.ScoreExplanation;
 import org.optaplanner.core.api.score.ScoreManager;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
@@ -9,9 +10,12 @@ import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
 import org.optaplanner.core.config.solver.SolverConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,18 +29,24 @@ public class SolverController {
     ScheduleTable unsolvedScheduleTable;
 
     @PostMapping(path = "/")
-    public String solve(@RequestBody RequestModel request) {
-        ArrayList<Schedule> scheduleList = new ArrayList<>();
+    public ResponseEntity<ResponseModel> solve(@RequestBody RequestModel request) {
+        List<Schedule> scheduleList = new ArrayList<>();
         for (int i = 0; i < request.getTimeslots().size(); i++) {
-            scheduleList.add(new Schedule());
+            unsolvedScheduleTable.getSchedules().add(new Schedule());
         }
+        System.out.print(scheduleList);
         SolverFactory<ScheduleTable> solverFactory = SolverFactory.create(new SolverConfig()
-            .withSolutionClass(ScheduleTable.class).withEntityClasses(Schedule.class).withEasyScoreCalculatorClass(ScoreCalculator.class)
+            .withSolutionClass(ScheduleTable.class).withEntityClasses(Schedule.class)
+                .withConstraintProviderClass(ScheduleTableConstraintProvider.class)
+                .withTerminationSpentLimit(Duration.ofMinutes(1))
         );
         ScoreManager<ScheduleTable, HardSoftScore> scoreManager = ScoreManager.create(solverFactory);
         Solver<ScheduleTable> solver = solverFactory.buildSolver();
+        unsolvedScheduleTable.getPractitioners().addAll(request.getPractitioners());
+        unsolvedScheduleTable.getTimeslots().addAll(request.getTimeslots());
         ScheduleTable solvedScheduleTable = solver.solve(unsolvedScheduleTable);
-        return "Hello";
+        ResponseModel responseModel = new ResponseModel(solvedScheduleTable.getSchedules());
+        return new ResponseEntity<>(responseModel, HttpStatus.OK);
     }
 
 }
